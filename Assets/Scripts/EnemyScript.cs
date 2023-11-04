488db8f3293aa1b93ecc16cfdf68f02516198825
Domantas Bacius <87576175+TisIDom@users.noreply.github.com>
2023-11-04 23:42:24 +0200

    Added shooting mechanic
    Added shooting and a basic enemy to test and build upon

---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float health = 100.0f; // Enemy's initial health
+
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+
+        if (health <= 0)
+        {
+            Die(); // Implement death logic if health reaches zero
+        }
+    }
+
+    void Die()
+    {
+        // Handle enemy's death, e.g., play death animation, spawn items, etc.
+        Destroy(gameObject); // Destroy the enemy
+    }
+}
