27d0506d1e43989a2cb7eb00d6125a763e0d23d9
Domantas Bacius <87576175+TisIDom@users.noreply.github.com>
2023-11-29 21:44:17 +0200

    Revert "Merge branch 'main' into in-level-upgrades"
    This reverts commit ba8efab926d78d533c5a7dd2c09544416123f97e, reversing
    changes made to 275fcae5a75b8b370ddd51a3a1fae0d2b5abfbd8.

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -6,32 +6,26 @@ using UnityEngine;
 public class EnemieMovement : MonoBehaviour
 {
     [SerializeField] private Transform player;
+
     [SerializeField] private float speed;
+
     [SerializeField] private float StopDistance;
+
     [SerializeField] private float ReturnDistance;
 
     public bool Flee = false;
-    public AudioClip enemyHitSound; // Add this field to assign the enemy hit sound
-    private AudioSource audioSource;
-
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
-        audioSource = GetComponent<AudioSource>();
-        if (audioSource == null)
-        {
-            // If AudioSource is not found, add one
-            audioSource = gameObject.AddComponent<AudioSource>();
-        }
     }
 
     // Update is called once per frame
     void Update()
     {
         float distance = Vector2.Distance(transform.position, player.position);
-
-        if (distance <= StopDistance) return;
+        
+        if(distance <= StopDistance) return;
 
         if (!Flee)
         {
@@ -49,18 +43,12 @@ public class EnemieMovement : MonoBehaviour
         if (col.gameObject.CompareTag("Player"))
         {
             PlayerScript player = col.gameObject.GetComponent<PlayerScript>();
-
+            
             if (player != null)
             {
                 player.TakeDamage(this.gameObject.GetComponent<Enemy>().damage);
-
-                // Play enemy hit sound
-                if (audioSource != null && enemyHitSound != null)
-                {
-                    audioSource.PlayOneShot(enemyHitSound);
-                }
             }
-
+            
             Flee = true;
         }
     }
