0afaab6cc4e6314692d7bef5418e5f2e8f852bb7
ignasvoveriukas9 <ignas.voveriukas123@gmail.com>
2023-11-29 15:29:10 +0200

    victory

--- Assets/Scripts/Tutorial/TutorialManager.cs
+++ Assets/Scripts/Tutorial/TutorialManager.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class TutorialManager : MonoBehaviour
+public class TutorialManager : MonoBehaviour, IDataPersistence
 {
     [SerializeField] private GameObject beginningScreen;
     [SerializeField] private GameObject xpScreen;
     [SerializeField] private GameObject goldScreen;
     [SerializeField] private GameObject levelUPScreen;
+    [SerializeField] private GameObject victoryScreen;
     [SerializeField] private GameObject gun;
     [SerializeField] private GameObject dummyEnemy;
     [SerializeField] private GameObject dummyEnemy2;
+    [SerializeField] private GameObject dummyEnemy3;
 
     private bool beginning = true;
     private bool killEnamyXp = false;
@@ -18,7 +21,10 @@ public class TutorialManager : MonoBehaviour
     private bool levelUP = false;
 
     private bool enemy2 = false;
-    
+    private bool enemy3 = false;
+
+    private bool complete;
+
     private void OnEnable()
     {
         EnemyTutorial.OnEnemyDeath += KillEnemy;
@@ -29,15 +35,33 @@ public class TutorialManager : MonoBehaviour
         EnemyTutorial.OnEnemyDeath -= KillEnemy;
     }
     
+    public void LoadData(GameData data)
+    {
+        complete = data.levelUnlock[1];
+    }
+    
+    public void SaveData(ref GameData data)
+    {
+        if (complete)
+        {
+            data.levelUnlock[1] = true;
+        }
+    }
+
+    
     // Start is called before the first frame update
     void Start()
     {
         beginningScreen.SetActive(beginning);
         xpScreen.SetActive(false);
+        goldScreen.SetActive(false);
+        levelUPScreen.SetActive(false);
+        victoryScreen.SetActive(false);
         Time.timeScale = 0;
         gun.SetActive(false);
         dummyEnemy.SetActive(false);
         dummyEnemy2.SetActive(false);
+        dummyEnemy3.SetActive(false);
     }
 
     // Update is called once per frame
@@ -86,6 +110,15 @@ public class TutorialManager : MonoBehaviour
             levelUPScreen.SetActive(true);
             Time.timeScale = 0;
             levelUP = true;
+            enemy2 = false;
+            enemy3 = true;
+        }
+        else if (enemy3)
+        {
+            victoryScreen.SetActive(true);
+            Time.timeScale = 0;
+            complete = true;
+            DataPersistenceManager.instance.SaveGame();
         }
         else
         {
@@ -94,5 +127,10 @@ public class TutorialManager : MonoBehaviour
             Time.timeScale = 0;
             enemy2 = true;
         }
+    }
+    
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
     }
 }
